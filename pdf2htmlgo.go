a8585002b2d7ac1d8727f30e4b615aa7bf3c2482
Maurizio De Santis <desantis.maurizio@gmail.com>
2013-06-11 18:11:52 +0200

    jessevdk/go-flags?

--- pdf2htmlgo.go
+++ pdf2htmlgo.go
@@ -1,8 +1,24 @@
 package main
 
 import (
+	"fmt"
 	"github.com/jessevdk/go-flags"
+	"os"
+	"strings"
 )
 
+var opts struct {
+	Version []bool `short:"v" long:"version" description:"print copyright and version info"`
+}
+
 func main() {
+	// Parse flags
+	args, err := flags.Parse(&opts)
+
+	if err != nil {
+		os.Exit(1)
+	}
+
+	fmt.Printf("Version: %d\n", len(opts.Version))
+	fmt.Printf("Remaining args: %s\n", strings.Join(args, " "))
 }
